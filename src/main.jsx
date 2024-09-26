ee302c47e00e7a7ab595f34db9818cf5f305a66b
Alinesa <alinesa322@gmail.com>
2024-09-26 23:11:09 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+    <Toaster />
+  </BrowserRouter>
+);
---
+++ src/sersices/api.js
@@ -0,0 +1,4 @@
+import axios from "axios";
+
+const defaultImg =
+  "https://dummyimage.com/400x600/cdcdcd/000.jpg&text=No+poster";
